59876b811d587d2c1dbbe94bedd2f3ceecc521cb
xlsn0w <xlsn0wios@gmail.com>
2018-07-31 21:36:59 +0800

    add

---
+++ objc_cpp_Nature/ImageDownloader.h
@@ -0,0 +1,13 @@
+//
+//  ImageDownloader.h
+//  objc_cpp_Nature
+//
+//  Created by HL on 2018/7/31.
+//  Copyright © 2018年 XL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageDownloader : NSObject
+
+@end
